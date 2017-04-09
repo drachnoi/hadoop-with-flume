edecf36a50874b3ff121bd3f09f42b0d9bbef7aa
drachnoi <drachnoi@gmail.com>
2017-04-09 14:00:21 +0200

    Handle flume configuration with hiera

--- provision/private-modules/flume/manifests/init.pp
+++ provision/private-modules/flume/manifests/init.pp
@@ -1,4 +1,9 @@
-class flume {
+class flume (
+  $consumer_key        = undef,
+  $consumer_key_secret = undef,
+  $access_token        = undef,
+  $access_token_secret = undef,
+  ){
 
   exec { 'download flume' :
     command => 'wget -O /tmp/flume.tar.gz http://xenia.sote.hu/ftp/mirrors/www.apache.org/flume/1.7.0/apache-flume-1.7.0-bin.tar.gz',
@@ -15,5 +20,10 @@ class flume {
     group   => $user,
     creates => "${flume_home}-1.7.0",
     require => Exec['download flume'],
+  }
+
+  file { '/home/hadoop/apache-flume-1.7.0-bin/conf/flume-conf.properties':
+    ensure  => file,
+    content => template('flume/flume-conf.properties.erb'),
   }
 }
